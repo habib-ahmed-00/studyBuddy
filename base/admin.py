94d7a174cd1a46e20b0ffb849a66e53f3e23f7a8
habib-ahmed-00 <hahmed.y2k@gmail.com>
2022-08-04 16:26:14 +0530

    Study Buddy project done

--- base/admin.py
+++ base/admin.py
@@ -1,19 +1,10 @@
 from django.contrib import admin
-from .models import *
-
-# @admin.register(Message)
-class messageAdmin(admin.ModelAdmin):
-    list_display = ('user', 'room', 'body')
-
-# @admin.register(Room)
-class roomAdmin(admin.ModelAdmin):
-    list_display = ('name', 'topic', 'host')
-
-# @admin.register(Topic)
-class topicAdmin(admin.ModelAdmin):
-    list_display = ('name',)
 
 # Register your models here.
-admin.site.register(Room, roomAdmin)
-admin.site.register(Topic, topicAdmin)
-admin.site.register(Message, messageAdmin)
+
+from .models import Room, Topic, Message, User
+
+admin.site.register(User)
+admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
